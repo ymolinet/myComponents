e4034450a0b02d88c561be4059ac1ded7482c064
Yannick MOLINET <yannick@adn-systemes.fr>
2021-01-26 12:54:32 +0100

    Fix X-Auth-Header Update Method GetAccounts and GetSummaryAccount

--- myComponents.Wazo/WazoHelper.cs
+++ myComponents.Wazo/WazoHelper.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 
 namespace myComponents.Wazo
 {
     public class WazoHelper
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         private HttpClient _client;
-        private AuthenticationData _auth;
+        private AuthenticationCredentials _auth;
         public WazoHelper(string BaseURL)
         {
             _client = new HttpClient();
@@ -22,6 +28,9 @@ namespace myComponents.Wazo
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //_client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+            //_client.DefaultRequestHeaders.Add("Keep-Alive", "false");
+            _client.Timeout = TimeSpan.FromMinutes(20);
         }
 
         public void Dispose()
@@ -31,24 +40,77 @@ namespace myComponents.Wazo
 
         public bool Login(string Username, string Password)
         {
-            _auth = new AuthenticationData() { username = Username, password = Password };
-            HttpResponseMessage response = _client.PostAsJsonAsync("/api/auth/0.1/token", _auth).Result;
+            _auth = new AuthenticationCredentials() { username = Username, password = Password };
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _auth.authenticationString);
+            AuthenticationRequest _req = new AuthenticationRequest() { expiration = 900 };
+ 
+            HttpResponseMessage response = _client.PostAsJsonAsync("/api/auth/0.1/token", _req).Result;
+            
             if (response.IsSuccessStatusCode)
             {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Debug(message);
                 AuthenticationResponse authenticationResponse = response.Content.ReadAsAsync<AuthenticationResponse>().Result;
-                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("X-Auth-Token", authenticationResponse.token);
+                _client.DefaultRequestHeaders.Add("X-Auth-Token", authenticationResponse.data.token);
                 return true;
-            } else
+            }
+            else
+            {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Error(message);
                 return false;
+            }
         }
 
+        /// <summary>
+        /// Ref API : /api/confd/1.0/accounts/summary
+        /// </summary>
+        /// <param name="tenant_uuid">Pass in web header</param>
+        /// <returns></returns>
         public SummaryAccountDetail GetSummaryAccount(string tenant_uuid)
         {
-            var response = _client.PostAsJsonAsync("/api/accounts/summary", tenant_uuid).Result;
+            // var response = _client.GetAsync("/api/confd/1.0/accounts/summary").Result;
+            // _client.DefaultRequestHeaders.Remove("Wazo-Tenant");
+            HttpRequestMessage httpRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(_client.BaseAddress + "/api/confd/1.0/accounts/summary")
+            };
+            httpRequest.Headers.Add("Wazo-Tenant", tenant_uuid);
+            var response = _client.SendAsync(httpRequest).Result;
             if (response.IsSuccessStatusCode)
+            {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Debug(message);
                 return response.Content.ReadAsAsync<SummaryAccountDetail>().Result;
+            }
             else
+            {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Error(message);
                 return null;
+            }
+        }
+
+        /// <summary>
+        ///  Ref API : /api/confd/1.0/accounts
+        /// </summary>
+        /// <returns></returns>
+        public AccountsListReponse GetAccounts()
+        {
+            var response = _client.GetAsync("/api/confd/1.0/accounts").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Debug(message);
+                return response.Content.ReadAsAsync<AccountsListReponse>().Result; ;
+            }
+            else
+            {
+                string message = response.Content.ReadAsStringAsync().Result;
+                logger.Error(message);
+                return null;
+            }
         }
     }
 }
--- myComponents.Wazo/WazoObject.cs
+++ myComponents.Wazo/WazoObject.cs
@@ -1,31 +1,93 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Text;
 
 namespace myComponents.Wazo
 {
-    public class AuthenticationData
+    public class AuthenticationCredentials
     {
         public string username { get; set; }
         public string password { get; set; }
+
+        public string authenticationString { get { return Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")); } }
     }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class AuthenticationRequest
+    {
+        public string access_type { get; set; }
+        public string backend { get; set; }
+        public int expiration { get; set; }
+
+        public string client_id { get; set; }
+
+        public string refresh_token { get; set; }
+    }
+
+
     public class AuthenticationResponse
     {
+        public AuthenticationData data { get; set; }
+    }
+
+    public class AuthenticationData
+    {
+        public string[] acl { get; set; }
+        public string[] acls { get; set; }
+        public string auth_id { get; set; }
+        public string expires_at { get; set; }
+        public string issued_at { get; set; }
+        // public string metadata { get; set; }
+        public string session_uuid { get; set; }
         public string token { get; set; }
+        public string utc_expires_at { get; set; }
+        public string utc_issued_at { get; set; }
+        public string xivo_user_uuid { get; set; }
+        public string xivo_uuid { get; set; }
     }
 
     public class LicenceDetail
     {
         public Int32 total { get; set; }
     }
+
     public class SummaryAccountDetail
     {
-        [JsonProperty("1")]
+        [JsonProperty("subscriptions")]
+        public Subscriptions Subscriptions { get; set; }
+    }
+    public class Subscriptions
+    {
+        [JsonProperty("0")]
         public LicenceDetail Voice { get; set; }
-        [JsonProperty("2")]
+        [JsonProperty("1")]
         public LicenceDetail UnifiedCommunication { get; set; }
-        [JsonProperty("3")]
+        [JsonProperty("2")]
         public LicenceDetail Collaboration { get; set; }
-        [JsonProperty("4")]
+        [JsonProperty("3")]
         public LicenceDetail CustomerRelationShip { get; set; }
+    }
+
+    public class AccountsListReponse
+    {
+        public Account[] items { get; set; }
+        public int filtered { get; set; }
+        public int total { get; set; }
+    }
+    public class Account
+    {
+        public string created_at { get; set; }
+        public string modified_at { get; set; }
+        public int subscription { get; set; }
+        public string tenant_uuid { get; set; }
+        public string uuid { get; set; }
+
+        public customer customer { get; set; }
+    }
+
+    public class customer
+    {
+        public string name { get; set; }
+        public string uuid { get; set; }
     }
 }
